2693f2d998e9462fe272d3375e88f725a149cc76
unknown <azuser2557_mml.local@techademy.com>
2025-01-27 07:56:52 +0000

    restapi profile app created

---
+++ profileapp/profileapp/src/main/java/com/finaltask/profileapp/ProfileappApplication.java
@@ -0,0 +1,13 @@
+package com.finaltask.profileapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileappApplication.class, args);
+	}
+
+}
---
+++ profileapp/profileapp/src/test/java/com/finaltask/profileapp/ProfileappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finaltask.profileapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
