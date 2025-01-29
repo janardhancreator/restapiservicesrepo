0e4b0f324b09b99b854b7e2cae3e11ee5d32945c
azuser2557_mml.local <azuser2557_mml.local@techademy.com>
2025-01-29 13:56:29 +0000

    updated file

--- profileapp/profileapp/src/main/java/com/finaltask/profileapp/repository/ContactRepository.java
+++ profileapp/profileapp/src/main/java/com/finaltask/profileapp/repository/ContactRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.finaltask.profileapp.model.Contact;
 
 
-
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
 }
--- profileapp/profileapp/src/main/java/com/finaltask/profileapp/repository/ExpertiseRepository.java
+++ profileapp/profileapp/src/main/java/com/finaltask/profileapp/repository/ExpertiseRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.finaltask.profileapp.model.Expertise;
 
 
-
 public interface ExpertiseRepository extends JpaRepository<Expertise, Long> {
 
 }
